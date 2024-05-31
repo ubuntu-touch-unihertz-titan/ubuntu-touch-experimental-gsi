55edffc83e6960e22cafdcf62d42bec5a7707a2d
TheKit <thekit@disroot.org>
2024-05-31 00:12:15 +0000

    .gitlab-ci.yml/build.sh: use the 'main' branch of scripts

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -xe
 
-[ -d build ] || git clone -b halium-12 https://gitlab.com/ubports/community-ports/halium-generic-adaptation-build-tools build
+[ -d build ] || git clone https://gitlab.com/ubports/community-ports/halium-generic-adaptation-build-tools build
 ./build/build.sh "$@"
